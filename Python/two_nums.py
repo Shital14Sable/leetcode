db0ba590ee9a9fc6a0852add47fc62a1532d8bbf
Shital <shital14sable@gmail.com>
2024-01-15 21:22:23 -0700

    New Structure

--- Python/two_nums.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            req = target-nums[i]
-            if req in nums[i+1:]:
-                ind = (nums[i+1:]).index(req)  + i + 1
-                return [ind, i]
